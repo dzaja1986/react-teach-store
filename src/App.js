b3e1e9b5b172cdf57ae27c13614420da965b0728
Milan Miloradovic <milan.miloradovic@cybercom.com>
2019-07-03 10:17:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <h>hello from tech store</h>
+    )
+  }
+}
+
+export default App;
